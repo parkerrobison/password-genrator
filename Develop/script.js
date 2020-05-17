17d772788b1d245027b1ce078c3b7a024429337b
probison <parker.robison@hotmail.com>
2020-05-16 23:29:46 -0600

    added prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,63 +1,75 @@
 // Assignment code here
-
-var lChar = ("abcdefghijklmnopqrstuvwxyz");
-var uChar = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-var num = ("0123456789");
-//num = num.split("");
-var sym = ("!@#$%^&*()'/-_=+{[}],.<>?|\"\\");
-// clicking the button generates prompts
-
-//set each prompt value to a variable
-
+var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '\'', '`', '{', '|', '}', '~', '"', '\\'];
 
 function generatePassword() {
   var passwordOptions = [];
-  var passwordLength = window.prompt("How long will your password be? Please enter a value between 8 and 128.");
+  var passwordLength = 0;
+  var newPasssword = "";  
 
-// 1)whenever you get input check it for errors. (if password length is >128 or <8)
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = window.prompt("How long will your password be? Please enter a value between 8 and 128.");
+    if (passwordLength < 8 || passwordLength > 128)  {
+      window.alert("This is not a valid answer. Please enter a value between 8 and 128.");
+    }
+  }
 
-var passwordQuestions2 = window.confirm("Will your password include lower case letters?"); //if is true push into an array
-if (passwordQuestions2) {
-    passwordOptions.push("lower");
-}
+  // if confirm is true, an array will be added to passwordOptions
+  while (passwordOptions.length === 0) {
+    var optionLower = window.confirm("Will your password include lower case letters? Click 'Okay' for yes and 'Cancel' for no."); 
+    if (optionLower) {
+      passwordOptions.push(lower);
+    }
 
-var passwordQuestions3 = window.confirm("Will your password include Upper case letters?");
-if (passwordQuestions3) {
-  passwordOptions.push("upper");
-}
-var passwordQuestions4 = window.confirm("Will your password include numbers?");
-if (passwordQuestions4) {
-    passwordOptions.push("number");
-}
-var passwordQuestions5 = window.confirm("Will your password include symbols?");
-if (passwordQuestions5) {
-    passwordOptions.push("symbol");
-}
-console.log(passwordOptions);
-}
+    var optionUpper = window.confirm("Will your password include upper case letters? Click 'Okay' for yes and 'Cancel' for no."); 
+    if (optionUpper) {
+      passwordOptions.push(upper);
+    }
+
+    var optionNum = window.confirm("Will your password include numbers? Click 'Okay' for yes and 'Cancel' for no."); 
+    if (optionNum) {
+      passwordOptions.push(number);
+    }
+
+    var optionSym = window.confirm("Will your password include symbols? Click 'Okay' for yes and 'Cancel' for no."); 
+    if (optionSym) {
+      passwordOptions.push(symbols);
+    }
+    if (passwordOptions.length === 0) {
+      window.alert("No character types were selected. Please select at least on character type for your password.");
+      
+    }
+  }
+
+
+  //password criteria length, upper, lower, sym, num
+
+
+  //prompt validation
+
+
+  // 1 criteria requirement. 
 
 
 
-// prompts ask for password criteria
-
-// password length = 8-128
 
 
+
+ }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-if (generateBtn) {
-  var password = generatePassword(); //this needs to return a string
-  writePassword(password);
-}
 
 // Write password to the #password input
-function writePassword(password) { 
+function writePassword() {
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
